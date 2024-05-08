16ac4689f362bb0c8d749c203a258ff09921716f
Waldemar Rutkowski <waldemar.rutkowski@xebia.com>
2024-05-08 13:50:23 +0200

    fixes

--- src/SingleWebApplicationDefaultAuth/Program.cs
+++ src/SingleWebApplicationDefaultAuth/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SingleWebApplicationDefaultAuth;
 using SingleWebApplicationDefaultAuth.Code.Authentication;
+using SingleWebApplicationDefaultAuth.Code.HttpClients;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -14,6 +15,7 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore().AddScoped<AuthenticationStateProvider, AzureSwaAuthenticationStateProvider>();
 
-
+builder.Services.AddTransient<UserDabRoleHeader>();
+builder.Services.AddHttpClients(builder.Configuration);
 
 await builder.Build().RunAsync();
