dd76233632905a6dbd925d3aada5e89d933eccf6
Waldemar Rutkowski <waldemar.rutkowski@xebia.com>
2024-05-08 09:48:56 +0200

    AddCascadingAuthenticationState

--- src/SingleWebApplicationDefaultAuth/Program.cs
+++ src/SingleWebApplicationDefaultAuth/Program.cs
@@ -10,8 +10,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, AzureSwaAuthenticationStateProvider>();
+
+
 
 await builder.Build().RunAsync();
